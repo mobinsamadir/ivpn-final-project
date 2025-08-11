24c3f41516a2ab3286e606cde3a30518d93ec550
mobinsamadir <118572208+mobinsamadir@users.noreply.github.com>
2025-08-12 03:14:06 +0330

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,10 +1,15 @@
 // File: android/settings.gradle.kts
+// This is the final, correct version that explicitly adds the Flutter repository.
 
 pluginManagement {
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
+        // This line is essential for finding the Flutter plugin
+        maven {
+            url = uri("https://storage.googleapis.com/download.flutter.io")
+        }
     }
 }
 
@@ -13,6 +18,10 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        // This line is also needed for some Flutter packages
+        maven {
+            url = uri("https://storage.googleapis.com/download.flutter.io")
+        }
     }
 }
 
