4052a0c25a04a82768ddff8430009dc7bcde7a79
mobinsamadir <118572208+mobinsamadir@users.noreply.github.com>
2025-08-12 03:07:43 +0330

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,18 +1,11 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
+// File: android/build.gradle.kts
+
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
-
-rootProject.name = "android"
-include(":app")
