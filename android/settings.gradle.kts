d6b2144452d4f104d546605518d9abf6c6ee7985
mobinsamadir <118572208+mobinsamadir@users.noreply.github.com>
2025-08-17 03:05:28 +0330

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,29 +1,25 @@
-// File: android/settings.gradle.kts
-// This is the final, correct version that explicitly adds the Flutter repository.
-
 pluginManagement {
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        file("local.properties").inputStream().use { properties.load(it) }
+        val flutterSdkPath = properties.getProperty("flutter.sdk")
+        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+        flutterSdkPath
+    }
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
-        // This line is essential for finding the Flutter plugin
-        maven {
-            url = uri("https://storage.googleapis.com/download.flutter.io")
-        }
     }
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        // This line is also needed for some Flutter packages
-        maven {
-            url = uri("https://storage.googleapis.com/download.flutter.io")
-        }
-    }
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.3.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
 }
 
-rootProject.name = "android"
 include(":app")
